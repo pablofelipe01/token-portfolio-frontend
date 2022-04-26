57bf1c54c7a7cd9aff0c31623f47711c3b619383
Pablo Acebedo <pablofelipe@me.com>
2022-04-26 17:46:57 -0500

    navbar first part

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './Navbar.scss'
+import { images } from '../../constants'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+
+        <ul className="app__navbar-links">
+          {['home', 'about', 'work', 'skills', 'contact'].map((item)=> (
+            <li className="app__flex p-tex" key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}>{item}</a>
+            </li>
+
+          ))}
+        </ul>
+    </nav>
   )
 }
 
