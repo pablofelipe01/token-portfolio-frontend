915786e596f29751b44c0cfe96ae3baeceec5c22
Pablo Acebedo <pablofelipe@me.com>
2022-04-29 16:51:12 -0500

    About first part

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import { motion } from 'framer-motion'
 import './About.scss'
+
+const abouts = [
+  { title: 'Software Developer', description: 'I am a Grate developer', imgUrl: ''},
+  { title: 'Blockchain Developer', description: 'I am a Grate developer', imgUrl: ''},
+  { title: 'Tokenization Consultant', description: 'I am a Grate developer', imgUrl: ''},
+  { title: 'Smart Contracts', description: 'I am a Grate developer', imgUrl: ''},
+]
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <h2 className="head-text">
+      I know That
+      <span>Good Design</span>
+      <br />
+      Means 
+      <span>Good Business</span>
+
+    </h2>
+
+    <div className="app__profiles">
+      {abouts.map((about, index) => (
+        <motion.div
+          whileInView={{ opacity: 1 }}
+          whileInHover={{ scale: 1.1 }}
+          transition={{ duration: 0.5, type: 'tween' }}
+          className='app__profile-item'
+          key={about.title + index}
+        
+        
+        >
+          <img src={about.imgUrl}  alt={about.title} />
+          <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title} </h2>
+          <p className='p-text' style={{ marginTop: 10 }}>{about.description} </p>
+
+        </motion.div>
+      ))}
+
+
+    </div>
+    
+    
+    
+    
+    </>
   )
 }
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -72,7 +72,7 @@ const Header = () => {
 
       >
 
-        {[images.flutter, images.redux, images.sass].map((circle, index)=> (
+        {[images.redux, images.react, images.sass].map((circle, index)=> (
             <div className="circle-cmp app__flex" key={`circle-${index}`}>
               <img src={circle} alt="circle" />
 
