829645dbbc10f961f0a1e83958119963ca2a4977
Pablo Acebedo <pablofelipe@me.com>
2022-05-04 17:11:57 -0500

    Work Scss

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -78,8 +78,32 @@ const Work = () => {
                 </motion.div>
 
               </a>
+              <a href={work.codeLink} target="_blank" rel= "noreferr">
+
+                <motion.div
+                  whileInView={{ scale: [0, 1] }}
+                  whileHover={{ scale: [1, 0.9]}}
+                  transition={{ duration: 0.25 }}
+                  className="app__flex"
+                
+                >
+                  <AiFillGithub />
+
+                </motion.div>
+
+              </a>
 
             </motion.div>
+
+          </div>
+
+          <div className="app__work-content app__flex">
+            <h4 className='bold-text'>{work.title}</h4>
+            <p className='p-text' style={{ marginTop: 10}}>{work.description}</p>
+
+            <div className="app__work-tag app__flex">
+            <p className='p-text'>{work.tags[0]} </p>
+            </div>
 
           </div>
 
@@ -95,4 +119,4 @@ const Work = () => {
   )
 }
 
-export default Work
+export default AppWrap(Work, 'work')
