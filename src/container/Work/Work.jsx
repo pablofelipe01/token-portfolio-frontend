e28d7959b05b49cc9b506dfd065243d7ce36fc66
Pablo Acebedo <pablofelipe@me.com>
2022-05-05 17:01:54 -0500

    Skills

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -24,7 +24,20 @@ const Work = () => {
   
 
   const handleWorkFilter = (item) => {
+    setActiveFilter(item)
+    setAnimateCard([{y: 100, opacity: 0}])
+    
+    setTimeout(() => {
+      setAnimateCard([{y: 0, opacity: 1}])
 
+      if(item === 'ALL') {
+        setFilterWork(works)
+
+      }else{
+        setFilterWork(works.filter((work) => work.tags.includes(item)))
+      }
+      
+    }, 500);
   }
   return (
     <>
