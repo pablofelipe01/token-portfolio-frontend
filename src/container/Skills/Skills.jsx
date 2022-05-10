a817923df416eb6c3298c4764fbcafaf212a3967
Pablo Acebedo <pablofelipe@me.com>
2022-05-10 16:45:33 -0500

    skills and experience

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -18,6 +18,7 @@ const Skills = () => {
 
     client.fetch(query)
      .then((data) => {
+       console.log(data)
        setExperience(data)
        
      })
@@ -36,7 +37,7 @@ const Skills = () => {
 
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
-          {skills.map((skill) => (
+          {skills?.map((skill) => (
             <motion.div
               whileInView={{opacity: [0, 1]}}
               transition={{ duration:0.5}}
@@ -53,9 +54,57 @@ const Skills = () => {
 
             </motion.div>
           ))}
+        </motion.div>
 
+         
+        <motion.div className="app__skills-exp">
+          {experience?.map((experience) => (
+            <motion.div
+            className="app__skills-exp-item"
+            key={experience.year}
+            >
+              <div className="app__skills-exp-year">
+                <p className='bold-text'>{experience.year}</p>
+
+              </div>
+
+              <motion.div className='app__skills-exp-works'>
+                {experience.works.map((work) => (
+                   <>
+            <motion.div
+            whileInView={{opacity: [0, 1]}}
+            transition={{ duration:0.5}}
+            className="app__skills-exp-work"
+            data-tip 
+            data-for={work.name}
+            key={work.name}
+            >
+              <h4 className='bold-text'>{work.name}</h4>
+              <p className='p-text'>{work.company} </p>
+            </motion.div>
+            <ReactTooltip
+              id={work.name}
+              effect='solid'
+              arrowColor='#fff'
+              className='skills-tooltip'
+            >
+              {work.desc}
+
+            </ReactTooltip>
+
+            </> 
+
+                ))}
+
+
+              </motion.div>
+
+            </motion.div>
+          ))}
+            
 
         </motion.div>
+            
       </div>
     
     
@@ -66,4 +115,9 @@ const Skills = () => {
   )
 }
 
-export default Skills
+export default AppWrap(Skills, 'skills') 
+             
+            
+            
+
+
