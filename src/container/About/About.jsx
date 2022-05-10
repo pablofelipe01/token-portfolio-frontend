381b587eeb28ab007dc33b69f8f4ff3ae49ee7d7
Pablo Acebedo <pablofelipe@me.com>
2022-05-10 17:52:29 -0500

    Scss

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion'
 // import { images } from '../../constants'
 import './About.scss'
 import { urlFor, client } from '../../client'
-import { AppWrap } from '../../wrapper'
+import { AppWrap, MotionWrap } from '../../wrapper'
 
 
 
@@ -52,4 +52,8 @@ const About = () => {
   )
 }
 
-export default AppWrap(About, 'about')
+export default AppWrap(
+  MotionWrap(About, 'app__about'),
+  'about',
+  'app__whitebg',
+);
--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
 import ReactTooltip from 'react-tooltip'
 
-import { AppWrap } from '../../wrapper'
+import { AppWrap, MotionWrap } from '../../wrapper'
 import { urlFor, client } from '../../client'
 import './Skills.scss'
 
@@ -115,7 +115,14 @@ const Skills = () => {
   )
 }
 
-export default AppWrap(Skills, 'skills') 
+
+
+
+export default AppWrap(
+  MotionWrap(Skills, 'app__skills'),
+  'skills',
+  'app__whitebg',
+);
              
             
             
